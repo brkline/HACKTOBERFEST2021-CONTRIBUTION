dbefcdf950007146aebeaec0b62ecf3ab50038c7
brkline <8756612+brkline@users.noreply.github.com>
2021-10-02 09:17:00 -0400

    Add Word Search in Text Files

---
+++ search_for_text_in_files.py
@@ -0,0 +1,30 @@
+# Searches text files for a specific word
+
+from pathlib import Path
+
+# Where are we searching and for what are we searching
+PATH_TO_SEARCH = '.'
+SEARCH_WORD = 'ipsum'
+
+""" 
+Read through all files with txt extension in the current
+path.  If we find a file that contains our search word,
+then print out the path.  You could also change the
+code below so it returns a list.
+
+"""
+
+
+def search_for_text():
+    for p in Path(PATH_TO_SEARCH).glob('*.txt'):
+        with p.open() as f:
+            if SEARCH_WORD in f.read():
+                print(f"{p.name}")
+
+
+def main():
+    search_for_text()
+
+
+if __name__ == '__main__':
+    main()
